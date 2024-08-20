fdd565c3033f9e10562044808171f376e0d1d1bb
Ankush-Goniya <ankushkumar03032@gmail.com>
2024-08-20 18:35:41 +0530

    Add files via upload

---
+++ AssignmentOperator.java
@@ -0,0 +1,32 @@
+public class AssignmentOperator {
+
+        public static void main(String[] args) {
+            int a = 10;
+            int b;
+
+            // =
+            b = a;
+            System.out.println(b); //10
+
+            // +=
+            a += b; // a = a + b,  10 + 10 = 20
+            System.out.println(a); //20
+
+            a-= b; // a = a - b , a = 20 - 10 = 10
+            System.out.println(a); //10
+
+            a *= b; // a = a * b  10*10=100
+            System.out.println(a); //100
+
+            a /= b; // a = a/b , a = 100/10 = 10
+            System.out.println(a); //10
+
+
+
+
+
+
+
+        }
+
+}
