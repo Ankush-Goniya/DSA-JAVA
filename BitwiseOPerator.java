ede4abc3e094557288e3cdd52abbbf63323397ef
Ankush-Goniya <ankushkumar03032@gmail.com>
2024-08-20 18:34:07 +0530

    Add files via upload

---
+++ BitwiseOPerator.java
@@ -0,0 +1,15 @@
+public class BitwiseOPerator {
+    public static void main(String[] args) {
+            int a = 10, b = 4;  // a=1010  b=0100
+
+            System.out.println(a & b);
+            System.out.println(a | b);
+            System.out.println(a ^ b);
+
+            System.out.println(a << 1); //10
+            System.out.println(b << 2); //20
+
+
+        }
+
+}
