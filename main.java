24e74eb10ffd2419fe7102de9f570b6198f722d0
Ankush-Goniya <ankushkumar03032@gmail.com>
2024-08-20 18:26:51 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,12 @@
+package input;
+import java.util.*;
+public class main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("taking input as an integer:");
+        int n=sc.nextInt();   //for taking 
+        System.out.println(n);
+
+    }
+
+}
