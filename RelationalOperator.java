f833e6e49431497eb710dc85d38f1511e1b8b4e6
Ankush-Goniya <ankushkumar03032@gmail.com>
2024-08-20 18:36:36 +0530

    Add files via upload

---
+++ RelationalOperator.java
@@ -0,0 +1,15 @@
+public class RelationalOperator {
+
+        public static void main(String[] args) {
+            int a = 20, b = 15;
+
+            System.out.println(a == b); //false
+            System.out.println(a != b); //true
+            System.out.println(a <= b); //false
+            System.out.println(a >= b); //true
+            System.out.println(a< b); //false
+            System.out.println(a > b); //true
+
+        }
+
+}
